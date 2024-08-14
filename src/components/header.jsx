71144330995d8aadc9d6d1e85db2ad624a066427
Mubashir1920 <131754637+Mubashir1920@users.noreply.github.com>
2024-08-14 12:16:15 +0500

    NavLink Added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,27 +1,26 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import ProfilePic from '../assets/mubashir-min.webp'
 import { IoMdClose } from "react-icons/io";
 import { motion } from 'framer-motion';
+import { Link,NavLink } from 'react-router-dom';
 
 
 const Header = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-
     return (
 
         <div className='container mx-auto'>
             <header className="bg-black p-4 flex  items-center justify-between">
-                <div className="flex items-center space-x-4">
+                <Link to='/' className="flex items-center space-x-4">
                     <img src={ProfilePic} alt="Avatar" className="w-10 h-10 rounded-full" />
-                    <Link to='/' className="text-xl text-white ">Mubashir | Portfolio</Link>
-                </div>
+                    <h1 className="text-xl text-white ">Mubashir | Portfolio</h1>
+                </Link>
                 <nav className="hidden font-normal text-white md:flex space-x-8">
-                    <Link to='/projects' className="relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Projects</Link>
-                    <Link to='/TechStack' className="relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">TechStack</Link>
-                    <Link to='/about' className="relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Experience</Link>
-                    <Link to='/contact' className="relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Contact</Link>
+                    <NavLink to='/projects' className="relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Projects</NavLink>
+                    <NavLink to='/TechStack' className="relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">TechStack</NavLink>
+                    <NavLink to='/about' className="relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Experience</NavLink>
+                    <NavLink to='/contact' className="relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Contact</NavLink>
                 </nav>
                 <Link to='/resume' className="relative hidden lg:block px-6 py-3 font-bold text-white rounded-lg group">
                     <span className="absolute inset-0 w-full h-full transition duration-300 transform -translate-x-1 -translate-y-1 bg-purple-800 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0"></span>
@@ -47,11 +46,11 @@ const Header = () => {
                     <div className='absolute top-16 right-10' >
                         <IoMdClose onClick={() => setIsMobileMenuOpen(false)} className=' text-white cursor-pointer' size={28} />
                     </div>
-                    <Link onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/projects' className="font-bold  relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Projects</Link>
-                    <Link onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/TechStack' className="font-bold relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">TechStack</Link>
+                    <NavLink onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/projects' className="font-bold  relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Projects</NavLink>
+                    <NavLink onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/TechStack' className="font-bold relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">TechStack</NavLink>
 
-                    <Link onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/about' className="font-bold  relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Experience</Link>
-                    <Link onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/contact' className="font-bold relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Contact</Link>
+                    <NavLink onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/about' className="font-bold  relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Experience</NavLink>
+                    <NavLink onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/contact' className="font-bold relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Contact</NavLink>
                     <Link onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/resume' className="relative px-6 py-3 font-bold text-white rounded-lg group">
                         <span className="absolute inset-0 w-full h-full transition duration-300 transform -translate-x-1 -translate-y-1 bg-purple-800 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0"></span>
                         <span className="absolute inset-0 w-full h-full transition duration-300 transform translate-x-1 translate-y-1 bg-red-800 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0 mix-blend-screen"></span>
