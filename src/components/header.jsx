5ba2e249e403ce2ed86240dddecf65ea30dbfb99
Mubashir1920 <131754637+Mubashir1920@users.noreply.github.com>
2024-09-09 09:22:52 +0500

    NavMenu Changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ProfilePic from '../assets/mubashir-min.webp'
 import { IoMdClose } from "react-icons/io";
-import { motion } from 'framer-motion';
-import { Link,NavLink } from 'react-router-dom';
+
+import { Link, NavLink } from 'react-router-dom';
 
 
 const Header = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const navref = useRef()
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (navref.current && !navref.current.contains(event.target)) {
+                setIsMobileMenuOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [])
 
     return (
 
@@ -33,31 +47,25 @@ const Header = () => {
                     <span className="block h-0.5 w-6 my-1 bg-white"></span>
                 </button>
             </header>
-            {isMobileMenuOpen &&
-                <motion.nav
-                    initial={{opacity:0,x:400}}
-                    animate={{opacity:1,x:0}}
-                    exit={{opacity:0 , x:400}}
-                    transition={{duration:0.2,ease:'easeInOut'}}
 
-                    id="mobile-menu"
-                    className="fixed flex flex-col items-center gap-7 justify-center text-xl inset-0 z-10 text-center w-full h-full bg-black/40 backdrop-blur-md text-white p-4 space-y-4"
-                >
-                    <div className='absolute top-16 right-10' >
-                        <IoMdClose onClick={() => setIsMobileMenuOpen(false)} className=' text-white cursor-pointer' size={28} />
-                    </div>
-                    <NavLink onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/projects' className="font-bold  relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Projects</NavLink>
-                    <NavLink onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/TechStack' className="font-bold relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">TechStack</NavLink>
+            <nav ref={navref}
+                className={`${isMobileMenuOpen ? 'translate-x-0 ' : 'translate-x-[1000px]'} rounded-3xl fixed flex flex-col items-center gap-7 justify-center transition-transform duration-300 text-xl top-0 z-10 text-center w-[80%] h-[100dvh] bg-black right-0  text-white p-4 space-y-4`}
+            >
+                <div className='absolute top-16 right-10' >
+                    <IoMdClose onClick={() => setIsMobileMenuOpen(false)} className='hover:rotate-180 duration-300 transition-transform text-white cursor-pointer' size={28} />
+                </div>
+                <NavLink onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/projects' className="font-bold  relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Projects</NavLink>
+                <NavLink onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/TechStack' className="font-bold relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">TechStack</NavLink>
 
-                    <NavLink onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/about' className="font-bold  relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Experience</NavLink>
-                    <NavLink onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/contact' className="font-bold relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Contact</NavLink>
-                    <Link onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/resume' className="relative px-6 py-3 font-bold text-white rounded-lg group">
-                        <span className="absolute inset-0 w-full h-full transition duration-300 transform -translate-x-1 -translate-y-1 bg-purple-800 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0"></span>
-                        <span className="absolute inset-0 w-full h-full transition duration-300 transform translate-x-1 translate-y-1 bg-red-800 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0 mix-blend-screen"></span>
-                        <span className="relative">Resume</span>
-                    </Link>
-                </motion.nav>
-            }
+                <NavLink onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/about' className="font-bold  relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Experience</NavLink>
+                <NavLink onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/contact' className="font-bold relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left ">Contact</NavLink>
+                <Link onClick={() => setIsMobileMenuOpen(prevState => !prevState)} to='/resume' className="relative px-6 py-3 font-bold text-white rounded-lg group">
+                    <span className="absolute inset-0 w-full h-full transition duration-300 transform -translate-x-1 -translate-y-1 bg-purple-800 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0"></span>
+                    <span className="absolute inset-0 w-full h-full transition duration-300 transform translate-x-1 translate-y-1 bg-red-800 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0 mix-blend-screen"></span>
+                    <span className="relative">Resume</span>
+                </Link>
+            </nav>
+
         </div>
 
 
